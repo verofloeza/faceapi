ebcfb2076ef05cbcbcb07a7535580a4a0fd32de4
verofloeza <verofloeza@gmail.com>
2025-03-05 19:44:14 -0300

    correccion

--- next.config.ts
+++ next.config.ts
@@ -15,21 +15,4 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['firebasestorage.googleapis.com'],
-  },
-  webpack(config) {
-    return config; // Deja que Next.js maneje la configuración de Webpack por defecto
-  },
-  "presets": ["next/babel"],
-  "plugins": [
-    ["import", { "libraryName": "antd", "style": "css" }]
-  ]
-};
-
-export default nextConfig;
 
