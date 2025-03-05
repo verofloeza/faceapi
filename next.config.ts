a151059ee3966fb9848deda6433237c7f355ae2b
verofloeza <verofloeza@gmail.com>
2025-03-05 18:12:42 -0300

    consfig

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,12 @@
 import type { NextConfig } from "next";
-/** @type {import('next').NextConfig} */
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [
-      'firebasestorage.googleapis.com',
-    ],
+    domains: ['firebasestorage.googleapis.com'],
   },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.css$/,
-      use: [ 'css-loader'],
-    });
-    return config;
+  webpack(config) {
+    return config; // Deja que Next.js maneje la configuración de Webpack por defecto
   },
   "presets": ["next/babel"],
   "plugins": [
@@ -22,3 +15,21 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['firebasestorage.googleapis.com'],
+  },
+  webpack(config) {
+    return config; // Deja que Next.js maneje la configuración de Webpack por defecto
+  },
+  "presets": ["next/babel"],
+  "plugins": [
+    ["import", { "libraryName": "antd", "style": "css" }]
+  ]
+};
+
+export default nextConfig;
+
