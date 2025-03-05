85e15a84105922fa2a6628c15e6daf98faa940a1
verofloeza <verofloeza@gmail.com>
2025-03-05 18:17:38 -0300

    correcciones

--- components/ui/use-toast.ts
+++ components/ui/use-toast.ts
@@ -16,12 +16,14 @@ type ToasterToast = ToastProps & {
   action?: ToastActionElement
 }
 
+/* eslint-disable @typescript-eslint/no-unused-vars */
 const actionTypes = {
   ADD_TOAST: "ADD_TOAST",
   UPDATE_TOAST: "UPDATE_TOAST",
   DISMISS_TOAST: "DISMISS_TOAST",
   REMOVE_TOAST: "REMOVE_TOAST",
-} as const
+} as const;
+/* eslint-enable @typescript-eslint/no-unused-vars */
 
 let count = 0
 
