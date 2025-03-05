b156c42f045de3489797448b59576e3f9bb66eb9
verofloeza <verofloeza@gmail.com>
2025-03-05 18:44:46 -0300

    calendar4

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -55,8 +55,8 @@ function Calendar({
       }}
       components={{
         // Usamos previousMonthButton y nextMonthButton para personalizar la navegación
-        PreviousButton: () => <ChevronLeft className="h-4 w-4" />,
-        NextButton: () => <ChevronRight className="h-4 w-4" />,
+        IconLeft: () => <ChevronLeft className="h-4 w-4" />,
+        IconRight: () => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
     />
